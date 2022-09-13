d7bde9e9b3b9c3ee47bd63639f8114e62d7f94c0
Ballahi <97371201+BROSTORNADO@users.noreply.github.com>
2022-09-13 15:15:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,17 +2,7 @@
 const inputEl = document.getElementById("input-el")
 const btnEl = document.getElementById("btn-el")
 const ulEl = document.getElementById("result")
-const collection = document.getElementsByClassName("conatiner");
-let vals = [
-   
-            270 /*whipping_cream 33.75*/,
-            135 /*icing_sugar 16.875*/,
-            150 /*whole_eggs 18.75*/,
-            30 /*cornstarch 3.75*/,
-            4 /*sea_salt 0.5*/,
-            1 /* vanila 0.125*/
-
-]
+const formEl = document.getElementById("form-id");
 
 
 
@@ -21,7 +11,8 @@ function go(){
 let number_of_people = parseInt(inputEl.value)
 
  ulEl.innerHTML = `
-    <p>Cream cheese : ${(number_of_people*67.5).toFixed(2)} pod</p> 
+
+    <p>Cream cheese : ${(number_of_people*67.5).toFixed(2)} pod</p>
     <p>Whipping cream : ${(number_of_people*33.75).toFixed(2)} g</p>
     <p>Icing sugar : ${(number_of_people*16.875).toFixed(2)} g</p>
     <p>Whole eggs : ${(number_of_people*18.75).toFixed(2)} g</p>
@@ -31,7 +22,8 @@ let number_of_people = parseInt(inputEl.value)
     <a href="reseted.html">reset</a>
 
 `
-
+ulEl.classList.remove("hidden");
+formEl.classList.add("hidden");
 }
 
 btnEl.addEventListener("click",function(){
